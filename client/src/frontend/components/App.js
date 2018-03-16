f22dab47fdee2dfe4263581860774c5e9a941dbc
gkopylov <gkopylov@team.amocrm.com>
2018-03-16 12:02:50 +0300

    init

---
+++ client/src/frontend/components/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    response: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => { 
+    const response = await fetch('api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  render() {
+    return (
+      <h1>
+        {this.state.response}
+      </h1>
+    );
+  }
+}
+
+export default App;
