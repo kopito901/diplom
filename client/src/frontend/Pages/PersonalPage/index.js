1e8fced77f18aabdf0a66dee8cc639445f683f66
gkopylov <gkopylov@team.amocrm.com>
2018-04-26 22:50:17 +0300

    small fix

--- client/src/frontend/Pages/PersonalPage/index.js
+++ client/src/frontend/Pages/PersonalPage/index.js
@@ -1,8 +1,6 @@
 import Base from './base';
 import Render from './render';
 
-import { connect } from 'react-redux';
-
 export default class PersonalPage extends Base {
   render() {
     return Render.call(this, this.props, this.state);
